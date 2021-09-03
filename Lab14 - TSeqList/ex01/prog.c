ae0fbe4c531951cd4b7dbd30fc6508c32bb8e7f6
travencolo <travencolo@gmail.com>
2021-09-03 11:57:04 -0300

    Lab14 - ex feito em aula

--- Lab14 - TSeqList/ex01/prog.c
+++ Lab14 - TSeqList/ex01/prog.c
@@ -1,5 +1,49 @@
 #include <stdio.h>
+#include "TSeqList.h"
+#include <string.h>
 
-int main(){
+int main()
+{
+    Lista *alunos_pet, *alunos_enade = NULL;
+    alunos_pet = cria_lista();
+    alunos_enade = cria_lista();
+
+    int matricula;
+    char nome[30];
+    float n1, n2, n3;
+
+    struct aluno aluno;
     
+    for (int i = 0; i < 3; i++)
+    {
+        printf("Dados do alunos:");
+        scanf("%d%s%f%f%f", &matricula, nome, &n1, &n2, &n3);
+
+        aluno.matricula = matricula;
+        strcpy(aluno.nome, nome);
+        aluno.n1 = n1;
+        aluno.n2 = n2;
+        aluno.n3 = n3;
+
+        int ret=insere_lista_inicio(alunos_pet, aluno);
+        if (ret == -1 ){
+            printf("\nErro: lista cheia");
+        }
+       // insere_lista_final(alunos_pet, aluno);
+        
+        if (aluno.n1 >60){
+            insere_lista_final(alunos_enade,aluno);
+        }
+    }
+
+    struct aluno a;
+    consulta_lista_pos(alunos_pet,1,&a);
+    
+    imprime_lista(alunos_pet);
+    printf("\nenade\n");
+    //imprime_lista(alunos_enade);
+
+    printf("\n mat %d",a.matricula);
+
+    libera_lista(alunos_pet);
 }
