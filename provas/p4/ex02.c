132593da2e76ffd9a6c4dd4977e6823112731a3e
OtavioMalta <83037014+OtavioMalta@users.noreply.github.com>
2021-10-29 20:43:42 -0300

    P4

--- provas/p4/ex02.c
+++ provas/p4/ex02.c
@@ -14,7 +14,7 @@ implementação de uma fila estática de uma lista circular?
 
 A diferença é que o vetor de acesso circular possui um ponteiro para o inicio da fila
 e um para o próximo espaço a ser preenchido. Dessa forma tanto as funções de inserção 
-quanto de remoção se tornam O(1), pois em uma fila estas funções acessam apenas as extremidades.
+quanto de remoção se tornam O(1), pois em uma fila estas funções acessam apenas essas extremidades.
 Na lista circular, há apenas um ponteiro apontando para o final da lista (já que a partir deste
 é possível acessar tanto o último quanto o primeiro elemento) assim funções de inserção e 
 remoção (exceto as no início e no final da fila) se tornam O(n), pois vão precisar percorrer 
@@ -43,13 +43,3 @@ struct aluno next_li(CircList *li){
     return al;
 }
 
-/*RETORNANDO INTEIROS PARA VERIFICAÇÕES DE ERROS
-int next_li(Lista *li, struct aluno *al){
-    if(li==NULL){return -1;}
-    if(li->next==NULL){return -2;}
-
-    *al = li->next->dados;
-    if(li->next->next == NULL){return -2;}
-    li->next = li->next->next;
-    return 0;
-}*/
