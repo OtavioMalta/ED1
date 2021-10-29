c6ceab61b37bd26ef7b1ff8f2ace547a6fbc4989
travencolo <travencolo@gmail.com>
2021-10-29 18:49:58 -0300

    Enunciado Prova 4

---
+++ provas/p4/ex01.c
@@ -0,0 +1,21 @@
+/* ex03
+Crie uma função que faz a junção de dois vetores inteiros. 
+A função deve ter como entrada os dois vetores e seus
+respectivos tamanhos. A alocação de espaço necessário para
+o vetor concatenado deve ser feita dentro da função 
+(a função então deve devolver para o programa principal 
+o vetor alocado e também o tamanho desse novo vetor). 
+A função deve retornar um valor inteiro: (0 - zero)
+se a junção ocorreu de forma correta; (-1) em caso de erro.
+Mostre o uso da função no programa principal. Não é
+permitido criar novos tipos de dados para a solução
+do exercício (ou seja, sem uso de structs).
+
+
+Vetor 1: [1, 5, 2]
+Vetor 2: [10, 30] 
+Concatenção Vetor 1, Vetor 2
+
+Vetor Concatenado = [ 1, 5, 2, 10, 30]
+
+*/
---
+++ provas/p4/ex02.c
@@ -0,0 +1,16 @@
+/* ex02
+Uma das utilidades das listas circulares dinâmicas é a possibilidade de percorrer
+todos os elementos por diversas vezes em um loop. Modifique o TAD para incluir uma
+função que retornará sempre o próximo elemento. Por exemplo, se a lista não foi
+percorrida ainda, o próximo elemento é a cabeça. Depois, ao chamar a função
+novamente, será devolvido o segundo elemento, e assim sucessivamente.
+(a) mostrar como fica a estrutura do TAD
+(b) mostrar a função que retorna o próximo elemento
+*Não é necessário mostrar as outras funções afetadas pela modificação proposta
+
+(c) Qual a diferença entre o vetor de acesso circular usado na 
+implementação de uma fila estática de uma lista circular?
+// responder aqui:
+
+
+*/
