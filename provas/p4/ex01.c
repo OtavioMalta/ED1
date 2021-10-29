aab57d843579a4abd316c7c4e789717e65b12feb
OtavioMalta <83037014+OtavioMalta@users.noreply.github.com>
2021-10-29 20:37:05 -0300

    P4

--- provas/p4/ex01.c
+++ provas/p4/ex01.c
@@ -19,3 +19,41 @@ Concatenção Vetor 1, Vetor 2
 Vetor Concatenado = [ 1, 5, 2, 10, 30]
 
 */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int concatena(char *str1, char *str2, int tam1, int tam2, char *resultado, int *tam3){
+    int Total = tam1 + tam2 + 1; // '/0'
+    char *str3 = malloc(Total*sizeof(char));
+    if(str3!=NULL){
+        int i, j;
+        for(i = 0; i<tam1; i++){
+            str3[i] = str1[i];
+        }
+        for(j = 0; j<tam2; j++){
+            str3[i] = str2[j];
+            i++;
+        }
+        strcpy(resultado, str3);
+        *tam3 = strlen(str3);
+        return 0;
+    }
+    else return -1;
+}
+
+void main(){
+    printf("Funcao Concatena\n\n");
+    char str1[3] = {1, 5, 2};
+    char str2[2] = {10, 30};
+    char *str3;
+    int tam3, res; 
+    res = concatena(str1, str2, 3, 2, str3, &tam3);
+    if(res == -1 ){printf("ERRO ao concatenar");}
+    printf("Vetor Concatenado: [ ");
+    for(int i = 0; i < tam3; i++){
+        printf("%d, ", str3[i]);
+    }
+    printf("]\n\n Tamanho do Vetor Concatenado: %d", tam3);
+}
--- provas/p4/ex03.c
+++ provas/p4/ex03.c
@@ -1,3 +1,4 @@
+/* 
 ex03) Mostre como uma pilha pode ser usada para resolver a expressão 
 matemática abaixo. Não confundir números negativos com subtrações.
 
@@ -11,3 +12,19 @@ Exemplo de como mostrar a pilha
 Expressão: 
 
 1 -8 6 * + 9 -
+
+1                   (TOPO: 1)
+1 -8                (TOPO: -8)
+1 -8 6              (TOPO: 6)
+1 (-8 * 6)
+1 -48               (TOPO: -48)
+(1 + -48)
+-47                 (TOPO: -47)
+-47 9               (TOPO: 9)
+(-47 - 9)
+-56                 (TOPO: -56)
+
+Resultado: -56
+
+/*
+
